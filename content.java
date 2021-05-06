bfc1dff682e443285c989cc2415f6f9b19bf3494
François Delbrayelle <fdelbrayelle@gmail.com>
2021-05-06 15:24:13 +0000

    add code in files types: .java,.xml remove code in files types: .java,.xml

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Ctkhub {
     public static void main() {
         System.out.println("bmnfc");
         System.out.println("yxmnn");
-        System.out.println("hfusw");
+        System.out.println("gllcm");
     }
 }
